d0841fc9f3564a0d714fdd57740c51572c2c1e0d
Hakan Yavuz <hknyvz_@hotmail.com>
2021-04-27 18:56:21 +0300

    Projenin oluşturulması

---
+++ ToDoApp/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { SafeAreaView, Text, StyleSheet, View } from 'react-native';
+
+
+const App = () => {
+
+  return (
+        <View>
+         
+        </View>
+  );
+}
+
+const styles=StyleSheet.create({
+ 
+})
+export default App;
